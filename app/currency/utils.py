75b08191de4f6f7fc112ac3cc09a167346801ed4
topolian <topolian.anna@gmail.com>
2023-09-10 21:49:33 +0900

    Initial commit

---
+++ app/currency/utils.py
@@ -0,0 +1,14 @@
+import random, string
+
+
+def generate_password(password_len: int = 10) -> str:
+    if not isinstance(password_len, int):
+        raise TypeError('Invalid Type...')
+
+    choices = string.ascii_letters + string.digits + '#$%&'
+    result = ''
+
+    for _ in range(password_len):
+        result += random.choice(choices)
+
+    return result
