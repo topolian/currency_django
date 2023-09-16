427e9c90c2b9b81e1761ab7b8135c57d0a60b744
topolian <topolian.anna@gmail.com>
2023-09-16 18:17:38 +0900

    Created Rate model, ContactUs model in currency app; added some views in currency app

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    sale = models.DecimalField(max_digits=5, decimal_places=2)
+    buy = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=32)
+    type = models.CharField(max_length=3)
+
+
+class ContactUs(models.Model):
+    email_from = models.EmailField(max_length=70)
+    subject = models.CharField(max_length=100)
+    message = models.TextField(max_length=700)
