294db36b924c5ec7ecfd4e2653489fbaaded8db7
topolian <topolian.anna@gmail.com>
2023-09-28 17:59:44 +0900

    added flake8 support and travis ci; created templates directory; added contacts_list() and rate_list() in views.py

--- app/currency/models.py
+++ app/currency/models.py
@@ -6,7 +6,7 @@ class Rate(models.Model):
     buy = models.DecimalField(max_digits=5, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     source = models.CharField(max_length=32)
-    type = models.CharField(max_length=3)
+    type = models.CharField(max_length=3)   # noqa
 
 
 class ContactUs(models.Model):
